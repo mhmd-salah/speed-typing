0297e6fac11745ba1ed7441d03d63eb88b4c8011
mhmd <ms.mhmdsalah@gmail.com>
2024-05-23 01:16:08 +0300

    end style

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+let words: string[] = [
+  "Hello",
+  "Programming",
+  "Code",
+  "javaScript",
+  "Town",
+  "Testing",
+  "Linkedin",
+  "Github",
+  "Python",
+  "Php",
+  "Java",
+  "Scala"
+];
