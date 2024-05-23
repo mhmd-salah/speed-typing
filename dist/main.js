5ec7aa46fe1f73a2b7ddaf3b4685a95dfbe3c694
mhmd <ms.mhmdsalah@gmail.com>
2024-05-23 03:33:09 +0300

    start ts code

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,43 @@
 "use strict";
+function sanitizeInput(input) {
+    return input.replace(/<[^>]*>?/gm, ''); // Removes HTML tags
+}
+let log = console.log;
+let words = [
+    "Hello",
+    "Programming",
+    "Code",
+    "javaScript",
+    "Town",
+    "Testing",
+    "Linkedin",
+    "Github",
+    "Python",
+    "Php",
+    "Java",
+    "Scala"
+];
+// settings lvls
+var Lvls;
+(function (Lvls) {
+    Lvls[Lvls["Easy"] = 5] = "Easy";
+    Lvls[Lvls["Normal"] = 3] = "Normal";
+    Lvls[Lvls["Hard"] = 2] = "Hard";
+})(Lvls || (Lvls = {}));
+// Default lvl
+let defaultLvlName = "Easy";
+let defaultLvlSeconds = Lvls[defaultLvlName];
+//catch selectors
+let startButton = document.querySelector(".start");
+let lvlNameSpan = document.querySelector(".message .lvl");
+let secondsSpan = document.querySelector(".message span.seconds");
+let theWord = document.querySelector(".the-word");
+let upcomingWords = document.querySelector(".upcoming");
+let input = document.querySelector(".input");
+let timeLeftSpan = document.querySelector(".time span");
+let scoreGot = document.querySelector(".score .got");
+let finshMessage = document.querySelector(".finish");
+// settings lvl name + seconds + score 
+lvlNameSpan.innerHTML = defaultLvlName;
+secondsSpan.innerHTML = String(defaultLvlSeconds);
+timeLeftSpan.innerHTML = String(defaultLvlSeconds);
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+function sanitizeInput(input:string) {
+    return input.replace(/<[^>]*>?/gm, ''); // Removes HTML tags
+}
+
+let log = console.log
+
 let words: string[] = [
   "Hello",
   "Programming",
@@ -12,3 +18,32 @@ let words: string[] = [
   "Java",
   "Scala"
 ];
+
+// settings lvls
+enum Lvls {
+  Easy = 5,
+  Normal = 3,
+  Hard = 2,
+}
+
+// Default lvl
+
+let defaultLvlName: keyof typeof Lvls = "Easy";
+let defaultLvlSeconds = Lvls[defaultLvlName];
+
+//catch selectors
+let startButton = document.querySelector(".start");
+let lvlNameSpan = document.querySelector(".message .lvl") as HTMLElement;
+let secondsSpan = document.querySelector(".message span.seconds") as HTMLElement;
+let theWord = document.querySelector(".the-word")
+let upcomingWords = document.querySelector(".upcoming")
+let input = document.querySelector(".input")
+let timeLeftSpan = document.querySelector(".time span") as HTMLElement
+let scoreGot = document.querySelector(".score .got")
+let finshMessage = document.querySelector(".finish")
+
+
+// settings lvl name + seconds + score 
+lvlNameSpan.innerHTML = defaultLvlName;
+secondsSpan.innerHTML = String(defaultLvlSeconds);
+timeLeftSpan.innerHTML = String(defaultLvlSeconds)
